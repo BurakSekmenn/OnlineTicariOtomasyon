b47e9ce196be9c52823918913fbe55756d139e49
Burak Sekmen <burakskmn07@gmail.com>
2023-08-04 01:51:16 +0300

    Departman crud complate

---
+++ MvcOnlineTicariOtomasyon/Controllers/DepartmanController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Departmans.Where(x=>x.Durum==true).ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult DepartmanEkle() 
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman k)
+        {
+            c.Departmans.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanSil(int id)
+        {
+            var urundeger = c.Departmans.Find(id);
+            urundeger.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanGetir(int id)
+        {
+            var deger = c.Departmans.Find(id);
+            return View("DepartmanGetir", deger);
+        }
+        public ActionResult DepartmanGuncelle(Departman k)
+        {
+            var ktgr = c.Departmans.Find(k.Departmanid);
+            ktgr.DepartmanAd = k.DepartmanAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanDetay(int id)
+        {
+            var degeler = c.Personels.Where(x=>x.Departmanid == id).ToList();
+            return View(degeler);
+
+        }
+
+    }
+}
