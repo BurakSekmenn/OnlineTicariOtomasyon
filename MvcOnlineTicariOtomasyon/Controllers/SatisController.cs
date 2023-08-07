b875ae5aba92cea21caf436fca4207d2a742bf8d
Burak Sekmen <burakskmn07@gmail.com>
2023-08-07 17:03:22 +0300

    Datatables uptade from table

--- MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -49,6 +49,51 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index"); 
         }
+        public ActionResult SatisGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           where x.Durum == true
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.Urunid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            List<SelectListItem> deger2 = (from a in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = a.Cariad + " " + a.CariSoyadı,
+                                               Value = a.Cariid.ToString()
+                                           }).ToList();
+            ViewBag.dgr2 = deger2;
+            List<SelectListItem> deger3 = (from b in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = b.PersonelAd + " " + b.PersonelSoyadı,
+                                               Value = b.Personelid.ToString()
+                                           }).ToList();
+            ViewBag.dgr3 = deger3;
+            var deger = c.SatisHarekets.Find(id);
+            return View(deger);
+        }
+        public ActionResult SatisGuncelle(SatisHareket sat)
+        {
+            var satgu = c.SatisHarekets.Find(sat.Satisid);
+            satgu.Cariid = sat.Cariid;
+            satgu.Adet = sat.Adet;
+            satgu.Fiyat = sat.Fiyat;
+            satgu.Personelid = sat.Personelid;
+            satgu.ToplamTutar = sat.ToplamTutar;
+            satgu.Tarih = sat.Tarih;
+            satgu.Urunid = sat.Urunid;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult SatisDetay(int id)
+        {
+            var degerler = c.SatisHarekets.Where(x=>x.Satisid == id).ToList();
+            return View(degerler);
+        }
 
     }
 }
