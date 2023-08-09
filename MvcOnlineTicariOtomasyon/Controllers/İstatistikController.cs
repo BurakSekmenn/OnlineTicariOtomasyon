b2adc1983353e92b263f2d7745d33c86b8d9145a
Burak Sekmen <burakskmn07@gmail.com>
2023-08-10 00:55:06 +0300

    galer page

--- MvcOnlineTicariOtomasyon/Controllers/İstatistikController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/İstatistikController.cs
@@ -35,6 +35,19 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             ViewBag.d10 = deger10;
             var deger11 = c.Uruns.Count(x => x.UrunAd == "Laptop").ToString();
             ViewBag.d11 = deger11;
+            var deger12 = c.Uruns.GroupBy(x=>x.Marka).OrderByDescending(z=>z.Count()).Select(y=>y.Key).FirstOrDefault();
+            ViewBag.d12=deger12;
+            var deger13 =c.Uruns.Where(u=>u.Urunid == c.SatisHarekets.GroupBy(x => x.Urunid).OrderByDescending(z => z.Count()).Select(y => y.Key).FirstOrDefault()).Select(k=>k.UrunAd).FirstOrDefault();
+            ViewBag.d13=deger13;
+            var deger14 = c.SatisHarekets.Sum(x => x.ToplamTutar).ToString();
+            ViewBag.d14=deger14;
+            var deger15 = c.SatisHarekets.Count(x => x.Tarih == DateTime.Today).ToString();
+            ViewBag.d15=deger15;
+            var deger16 = c.SatisHarekets
+               .Where(x => x.Tarih == DateTime.Today)
+               .Sum(x => (decimal?)x.Fiyat) ?? 0;
+
+            ViewBag.d16 = deger16;
             return View();
         }
     }
