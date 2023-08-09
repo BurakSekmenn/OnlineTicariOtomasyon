8d638345e055267c3914a299ec4eefcf48281993
Burak Sekmen <burakskmn07@gmail.com>
2023-08-09 15:49:08 +0300

    istatistik some refreshed

--- MvcOnlineTicariOtomasyon/Controllers/İstatistikController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/İstatistikController.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcOnlineTicariOtomasyon.Models.Sınıflar;
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class İstatistikController : Controller
     {
+        Context c = new Context();
         // GET: İstatistik
         public ActionResult Index()
         {
+            var deger1=c.Carilers.Count().ToString();
+            ViewBag.d1 = deger1;
+            var deger2=c.Uruns.Count().ToString();
+            ViewBag.d2 = deger2;
+            var deger3 = c.Personels.Count().ToString();
+            ViewBag.d3 = deger3;
+            var deger4 = c.Kategoris.Count().ToString();
+            ViewBag.d4 = deger4;
+            var deger5 = c.Uruns.Sum(x=>x.Stok).ToString();
+            ViewBag.d5=deger5;
+            var deger6 = c.Uruns.Count(x=>x.Stok<=20).ToString();
+            ViewBag.d6 = deger6;
+            var deger7 = (from x in c.Uruns select x.Marka).Distinct().Count().ToString();
+            ViewBag.d7=deger7;
+            var deger8 = (from x in c.Uruns orderby x.SatisFiyat descending select x.UrunAd).FirstOrDefault();
+            ViewBag.d8 = deger8;
+            var deger9 = (from x in c.Uruns orderby x.SatisFiyat ascending select x.UrunAd).FirstOrDefault();
+            ViewBag.d9 = deger9;
+            var deger10 = c.Uruns.Count(x => x.UrunAd =="Buzdolabı").ToString();
+            ViewBag.d10 = deger10;
+            var deger11 = c.Uruns.Count(x => x.UrunAd == "Laptop").ToString();
+            ViewBag.d11 = deger11;
             return View();
         }
     }
