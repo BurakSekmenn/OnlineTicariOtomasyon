18458ade6fc5b5765314f7e98f7dbabc0ce182a0
Burak Sekmen <burakskmn07@gmail.com>
2023-08-11 01:01:58 +0300

    new tempalte and urun details

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        // GET: UrunDetay
+        Context c = new Context();
+        public ActionResult Index(int id)
+        {
+            Class1 cs = new Class1();
+            cs.Deger1 = c.Uruns.Where(x=>x.Urunid == id).ToList();
+            cs.Deger2 = c.Detays.Where(y => y.DetayID == id).ToList();
+
+           
+
+            var urun = c.Uruns.Find(id);
+            var urunList = new List<Urun> { urun };
+            ViewBag.urunresim = urun.UrunGorsel;
+            ViewBag.urunfiyat = urun.SatisFiyat;
+            ViewBag.urunad = urun.UrunAd;
+            return View(urunList);
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Detay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Detay
+    {
+        [Key]
+        public int DetayID { get; set; }
+        [Column(TypeName = "VarChar")]
+        [StringLength(30)]
+        public string urunad { get; set; }
+        [Column(TypeName = "VarChar")]
+        [StringLength(2000)]
+        public string urunbilgi { get; set; }
+
+        
+    }
+}
