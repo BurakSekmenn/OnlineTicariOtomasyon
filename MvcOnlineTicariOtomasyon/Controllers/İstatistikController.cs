be8ef03ea3e8fd9637f40285b6ed6920d4859f83
Burak Sekmen <burakskmn07@gmail.com>
2023-08-23 23:45:27 +0300

    some problems finish

--- MvcOnlineTicariOtomasyon/Controllers/İstatistikController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/İstatistikController.cs
@@ -50,5 +50,11 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             ViewBag.d16 = deger16;
             return View();
         }
+        public ActionResult KolayTablolar() { 
+            
+            return View(); 
+        
+        }
+
     }
 }
