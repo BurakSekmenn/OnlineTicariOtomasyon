6b6b77262aff9e8fa14d176d6fc5c5f203666261
Burak Sekmen <burakskmn07@gmail.com>
2023-08-25 18:43:55 +0300

    Cari

--- MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
@@ -26,6 +27,27 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.Carilers.Add(t);
             c.SaveChanges();
             return PartialView();
+        }
+        [HttpGet]
+        public ActionResult CariLogin1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CariLogin1(Cariler ca)
+        {
+            var bilgiler= c.Carilers.FirstOrDefault(x=>x.CariMail == ca.CariMail && x.CariSifre==ca.CariSifre);
+            if (bilgiler!=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CariMail"] = bilgiler.CariMail.ToString();
+                return RedirectToAction("Index", "CariPanel"); 
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+         
         }
     }
 }
