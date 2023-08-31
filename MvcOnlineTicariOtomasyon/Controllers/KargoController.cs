0045de7b9149b5660da3f7d9d45bc9683a8673f5
Burak Sekmen <burakskmn07@gmail.com>
2023-09-01 01:30:36 +0300

    kargo model

---
+++ MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
@@ -0,0 +1,50 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class KargoController : Controller
+    {
+        // GET: Kargo
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var kargolar = c.KargoDetays.ToList();
+            return View(kargolar);
+        }
+        [HttpGet]
+        public ActionResult YeniKargo()
+        {
+            Random rnd = new Random();
+            string[] karaterler = { "A", "B", "C", "D" };
+            int k1, k2, k3;
+            k1 = rnd.Next(0, 4);
+            k2 = rnd.Next(0, 4);
+            k3 = rnd.Next(0, 4);
+            int s1, s2, s3;
+            s1 = rnd.Next(100, 1000);//100 --> 3 1 2 1 2 1
+            s2 = rnd.Next(10, 99);
+            s3 = rnd.Next(10, 99);
+            string kod = s1.ToString() + karaterler[k1] + s2 + karaterler[k2] + s3 + karaterler[k3];
+            ViewBag.takipkod = kod;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniKargo(KargoDetay k)
+        {
+            c.KargoDetays.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KargoTakip(string id)
+        {
+          
+            var degerler = c.kargoTakips.Where(x=>x.TakipKodu==id).ToList();
+            return View(degerler);
+        }
+    }
+}
