b08c7c8c0ffa6531a29d5ded53679046cb0b09f7
Burak Sekmen <burakskmn07@gmail.com>
2023-09-10 21:43:45 +0300

    fatura

--- MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using MvcOnlineTicariOtomasyon.Models.Sınıflar;
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
@@ -71,7 +73,28 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         }
         public ActionResult Dinamik()
         {
-            return View();
+            // class 2 de IEnumerable oluşturduk çünkü 2 farklı veritabanınndan veri alabilmek için .
+            Class2 cs = new Class2();
+            cs.deger1 = c.Faturalars.ToList();
+            cs.deger2 = c.FaturaKalems.ToList();
+            return View(cs);
+        }
+        public ActionResult FaturaKaydet(string FaturaSeriNo,string FaturaSıraNo,DateTime tarih,string VergiDairesi, string Saat, string TeslimEden,string TeslimAlan, string Toplam, FaturaKalem[] kalemler)
+        {
+           
+                Faturalar f = new Faturalar();
+                f.FaturaSerino = FaturaSeriNo;
+                f.FaturaSıraNo = FaturaSıraNo;
+                f.Tarih = tarih;
+                f.VergiDairesi = VergiDairesi;
+                f.Saat = Saat;
+                f.TeslimEden = TeslimEden;
+                f.TeslimAlan = TeslimAlan;
+                f.Toplam = decimal.Parse(Toplam);
+                c.Faturalars.Add(f);
+                c.SaveChanges();
+                return Json("İşlem Başarılı", JsonRequestBehavior.AllowGet);
+            
         }
     }
 }
